4ed73429b7bc1ba1251203a591d7d134f4798cd5
Hussam812 <hossamhosnihussien@gmail.com>
2023-09-15 05:11:37 +0200

    routes & Feed & Slidebar

---
+++ src/components/Feed.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
+
+const Feed = () => (
+  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+    <Box
+      sx={{
+        height: { sx: "auto", md: "92vh" },
+        borderRight: "1px solid #3d3d3d",
+        px: { sx: 0, md: 2 },
+      }}
+    >
+      <Sidebar />
+      <Typography
+        className="copyright"
+        varient="body2"
+        sx={{
+          mt: 1.5,
+          color: "#fff",
+        }}
+      >
+        Copyright 2022 Jsm Media
+      </Typography>
+    </Box>
+  </Stack>
+);
+
+export default Feed;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from "./Navbar";
+export { default as Feed } from "./Feed";
+export { default as ChannelDetail } from "./ChannelDetail.jsx";
+export { default as SearchFeed } from "./SearchFeed.jsx";
+export { default as VideoDetail } from "./VideoDetail.jsx";
+export { default as SearchBar } from "./SearchBar.jsx";
+export { default as Sidebar } from "./Sidebar.jsx";
