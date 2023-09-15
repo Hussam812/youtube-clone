7b8e95b3d0ec09e118374aff78bc8e7939204125
Hussam812 <hossamhosnihussien@gmail.com>
2023-09-15 07:44:30 +0200

    fetch api

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia, Box } from "@mui/material";
+import { ChickCircle } from "@mui/icons-material";
+import {
+  demoThumbnailUrl,
+  demoChannelUrl,
+  demoVideoUrl,
+  demoChannelTitle,
+  demoVideoTitle,
+} from "../utils/constants";
+
+const VideoCard = (video) => {
+  console.log(video);
+  return <div>VideoCard</div>;
+};
+
+export default VideoCard;
