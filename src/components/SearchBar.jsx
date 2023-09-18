95ab36035700279538416b437d16df8ff22d7d98
Hussam812 <hossamhosnihussien@gmail.com>
2023-09-18 03:35:39 +0200

    searchFeed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,29 +2,42 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
 import { Search } from "@mui/icons-material";
-const SearchBar = () => (
-  <Paper
-    component="form"
-    onSubmit={() => {}}
-    sx={{
-      borderRadius: 20,
-      border: "solid 1px #e3e3e3",
-      pl: 2,
-      boxShadow: "none",
-      mr: { sm: 5 },
-    }}
-  >
-    <input
-      type="text"
-      className="search-bar"
-      placeholder="Search..."
-      value={""}
-      onChange={() => {}}
-    />
-    <IconButton type="submit" sx={{ p: "20px", color: "red" }}>
-      <Search />
-    </IconButton>
-  </Paper>
-);
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+    }
+    setSearchTerm("");
+  };
+  return (
+    <Paper
+      component="form"
+      onSubmit={handelSubmit}
+      sx={{
+        borderRadius: 20,
+        border: "solid 1px #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        type="text"
+        className="search-bar"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <IconButton type="submit" sx={{ p: "20px", color: "red" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
 
 export default SearchBar;
